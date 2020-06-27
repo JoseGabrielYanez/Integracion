9269b38bc20b8d256d201a8a7d78178947379d6b
JoseGabrielYanez <jose.yanez@live.cl>
2020-06-26 20:10:07 -0400

    cambios en la funcion del formato csv

--- src/controlador/validaciones.js
+++ src/controlador/validaciones.js
@@ -1,21 +1,27 @@
 const formatos = require('moment');
-const csv = require('csv-stringify')
-const fs = require('fs')
+const csv = require('csv-stringify');
+const fs = require('fs');
+const ws = fs.createWriteStream('informacion.csv');
 
 const validaciones = {};
 const posiciones = [23, 14, 18, 19, 62, 67, 26, 68, 28, 10, 11, 81, 80, 6, 83, 30, 3, 4, 5, 48, 8, 2, 38, 39, 41, 42, 43, 46, 47, 13, 84, 12, 82];
 let archivo = [];
 let cuerpo = [];
 
+
 validaciones.validacion = function(arregloHead, arregloBody) {
 
     for (f = 0; f < arregloBody.length; f++) {
         for (c = 0; c < posiciones.length; c++) {
+
             indice = posiciones[c];
+
             if (arregloHead[indice].name == "FECHA_NAC" || arregloHead[indice].name == "FECHA_ENTREGA" || arregloHead[indice].name == 'FECHA_FACT') {
                 arregloBody[f][indice] = fecha(arregloBody[f][indice]);
             }
+
             cuerpo.push(arregloBody[f][indice]);
+
             if (!archivo.includes(arregloHead[indice].name))
                 archivo.push(arregloHead[indice].name);
         }
@@ -24,6 +30,7 @@ validaciones.validacion = function(arregloHead, arregloBody) {
 };
 
 function fecha(fechas) {
+
     if (fechas) {
         nuevo = formatos(fechas).format("YYYYMMDD");
     } else {
@@ -33,24 +40,16 @@ function fecha(fechas) {
 }
 
 function formatoCsv(header, datos) {
+
     input = [
         header,
         datos
     ]
-    csv(input, function(err, output) {
-        if (err) {
-            console.log(err);
-            return
-        }
-        CrearArchivo(output);
-    });
+    csv(input, {
+        delimiter: ';'
+    }).pipe(ws);
+
 }
 
-function CrearArchivo(archivo) {
-    fs.writeFile(`info.csv`, archivo, (err) => {
-        if (err) console.log(err);
-        console.log('archivo creado');
-    })
-}
 
 module.exports = validaciones;
