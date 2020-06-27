7155bf35ae376c584849e06c603df0107ae12abf
JoseGabrielYanez <jose.yanez@live.cl>
2020-06-27 15:39:13 -0400

    cambio en la funcion para crear csv

--- src/controlador/validaciones.js
+++ src/controlador/validaciones.js
@@ -1,53 +1,60 @@
-const formatos = require('moment');
-const csv = require('csv-stringify');
-const fs = require('fs');
-const ws = fs.createWriteStream('informacion.csv');
+const formatos = require('moment'); // cambiar los formatos de las fechas
+const fs = require('fs'); //  enviar el nombre del archivo
+const csvWriter = require('csv-write-stream'); //  crear el archivo csv con arreglos - por index
 
-const validaciones = {};
+const validaciones = {}; // metodo llamado desde controlador
+// posiciones que deben mantenerse de los arreglos. 
 const posiciones = [23, 14, 18, 19, 62, 67, 26, 68, 28, 10, 11, 81, 80, 6, 83, 30, 3, 4, 5, 48, 8, 2, 38, 39, 41, 42, 43, 46, 47, 13, 84, 12, 82];
-let archivo = [];
-let cuerpo = [];
+let arregloCabeceras = [];
+let arregloDatosLimpios = [];
 
 
 validaciones.validacion = function(arregloHead, arregloBody) {
 
     for (f = 0; f < arregloBody.length; f++) {
+        arregoPaso = []; // solo quedara con las posiciones deseadas.
         for (c = 0; c < posiciones.length; c++) {
-
             indice = posiciones[c];
 
             if (arregloHead[indice].name == "FECHA_NAC" || arregloHead[indice].name == "FECHA_ENTREGA" || arregloHead[indice].name == 'FECHA_FACT') {
-                arregloBody[f][indice] = fecha(arregloBody[f][indice]);
+                arregloBody[f][indice] = fecha(arregloBody[f][indice]); // llama a funcion fecha para cambio de formato
             }
 
-            cuerpo.push(arregloBody[f][indice]);
+            arregoPaso.push(arregloBody[f][indice]); // agrega una nueva posicion.
 
-            if (!archivo.includes(arregloHead[indice].name))
-                archivo.push(arregloHead[indice].name);
+            if (!arregloCabeceras.includes(arregloHead[indice].name))
+                arregloCabeceras.push(arregloHead[indice].name); // agrega los datos necearios y no repetidos para usarlas como cabecera.
         }
+        arregloDatosLimpios.push(arregoPaso); // va guardando arreglo filtrado
     }
-    formatoCsv(archivo, cuerpo);
+    formatoCsv(arregloCabeceras, arregloDatosLimpios); // funcion envia los 2 arreglos para generar el archivo CSV una vez filtrados
 };
 
 function fecha(fechas) {
 
     if (fechas) {
-        nuevo = formatos(fechas).format("YYYYMMDD");
+        nuevoFormato = formatos(fechas).format("YYYYMMDD");
     } else {
-        nuevo = ""
+        nuevoFormato = "";
     }
-    return nuevo
+    return nuevoFormato;
 }
 
 function formatoCsv(header, datos) {
 
-    input = [
-        header,
-        datos
-    ]
-    csv(input, {
-        delimiter: ';'
-    }).pipe(ws);
+    let writer = csvWriter({
+        separador: ';',
+        newline: '\n',
+        headers: header,
+        sendHeaders: true
+    });
+    writer.pipe(fs.createWriteStream('inscripcionCsv.csv')); // los datos se guardaran en este archivo
+
+    for (i = 0; i < datos.length; i++) { // recorre el arreglo para agregar cada fila al archivo de extencion csv
+        writer.write(datos[i]);
+    }
+
+    writer.end(); //cierra la edicion de archivo.
 
 }
 
